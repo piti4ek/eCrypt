597875b3784eac22081b758b45e1c63f5a0cce35
dev <piti4ek911@gmail.com>
2018-01-31 16:41:41 +0200

    add table right side

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// document.getElementById('ElemDrop').onclick = function() {
+//   MyFunc();
+// }
+//
+// function MyFunc() {
+//   var aria = ElemDrop.getAttribute('aria-expanded');
+//   if (aria == 'true') {
+//     document.getElementById('addClassBd').classList.add('active-two-select-side');
+//   }
+//
+//   else {
+//     document.getElementById('addClassBd').classList.add('active-one-select-side');
+//   }
+// }
